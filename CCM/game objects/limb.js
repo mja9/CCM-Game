086282c51878742e841f80cfcbdb4d51f3c9c540
mja9 <mja9@rice.edu>
2020-12-20 20:05:24 -0600

    Refactoring code

--- CCM/game objects/limb.js
+++ CCM/game objects/limb.js
@@ -283,3 +283,31 @@ class SaltIcon {
     }
 
 }
+
+
+class InterPosition extends GradientPosition{
+
+    constructor(xPos, yPos) {
+        super(xPos, yPos, 322, 82, 300);
+    }   
+
+    paint() {
+        // Draw rectangular positions.
+        this.colorFillMechanic();
+
+        // Draw numerical representation of concentration.
+        CONTEXT.fillStyle = "#252525";
+        CONTEXT.font = "40px Courier New";
+        CONTEXT.textAlign = "center";
+        CONTEXT.fillText(this.c.toString(), this.x, this.y);
+    }
+}
+
+class MaxBar {
+
+    constructor() {
+        
+    }
+
+
+}
